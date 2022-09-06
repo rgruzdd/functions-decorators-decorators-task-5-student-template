b27820a8fdd89b05a0646f2010f726169f76ba0f
Дмитрий Колб <dm.kolb@gmail.com>
2022-09-06 19:39:05 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,11 +1,11 @@
 
 def decorator_apply(lambda_func):
-  '''
-  Add your implementation here
-  '''
-  pass
+    '''
+    Add your implementation here
+    '''
+    pass
 
 
 @decorator_apply(lambda user_id: user_id + 1)
 def return_user_id(num: int) ->int:
-  return num
+    return num
