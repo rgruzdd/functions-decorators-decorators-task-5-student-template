8a63e3f7664b44dbfb465af23ac79c86be2a1da4
Gruzin <gruzd.roman@inbox.ru>
2023-04-11 15:53:35 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,11 +1,16 @@
 
 def decorator_apply(lambda_func):
-    '''
-    Add your implementation here
-    '''
-    pass
+    def wrapper(func):
+        def wrapper(x):
+            return func(lambda_func(x))
+        return wrapper
+    return wrapper
+
+
+
 
 
 @decorator_apply(lambda user_id: user_id + 1)
 def return_user_id(num: int) ->int:
     return num
+
